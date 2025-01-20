fe72ec8b7ddb77871e041b39f24c68bad20aa4ed
ashiq33 <107813420+ashiq33@users.noreply.github.com>
2025-01-20 22:35:17 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,8 +6,8 @@ export const config = {
         {
           heading: " E-commerce WebApp using React",
           description:
-            "Developed an intuitive e-commerce platform featuring core functionalities such as "Add to Bag,"
-            "Remove from Bag," auto-calculation of cart totals, and seamless navigation through the checkout process. 
+            "Developed an intuitive e-commerce platform featuring core functionalities such as Add to Bag,
+            Remove from Bag, auto-calculation of cart totals, and seamless navigation through the checkout process. 
             The application implements dynamic routes for pages like product listings, cart, 
           and payment, ensuring a smooth user experience. Leveraged Redux and React's useReducer for efficient state management, 
             while Node.js was used for server-side operations and data retrieval. This project provided valuable insights into
